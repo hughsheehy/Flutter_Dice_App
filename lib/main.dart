b2fa854acee98636fb26d8e309b856a705ec754c
hugh sheehy <hughsheehy@users.noreply.github.com>
2020-06-10 12:20:08 +0100

    readme update
    just changing the readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text("Hugh's Dice App"),
+          backgroundColor: Colors.teal,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+
+  void pushdicebutton(){
+    setState(() {
+      leftdicenumber = 1 + Random().nextInt(6);
+      rightdicenumber = 1 + Random().nextInt(6);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      pushdicebutton();
+                      },
+                    child: Image.asset('images/dice$leftdicenumber.png'),
+              )),
+              Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      pushdicebutton();
+                    },
+                   child: Image.asset('images/dice$rightdicenumber.png'),
+              )),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
